ed7c320be016e1f56373bb1ece63c802211a0a86
Gauri Vispute <gvispute@bluemeteor.com>
2025-08-07 13:04:08 +0530

    query on MySQL DB

--- query_on_DB.sql
+++ query_on_DB.sql
@@ -4,3 +4,16 @@ SELECT * FROM users;
 SELECT * FROM users WHERE gender ='FEMALE';
 
 select * from users where gender = 'Male';
+
+-- get entries of where gender not equal to male 
+select * from users where gender<>'Male'
+
+select * from users where gender != 'Male'--(not equal to male)
+
+select * from users where gender != 'Female' -- (not equal to female)
+
+select * from users where date_of_birth <'1995-09-09';
+
+select * from users where id>10;
+
+select * from users where id<=10;
