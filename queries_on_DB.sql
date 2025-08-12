8028879599da894056295b0d8ae8cab44259efb7
GauriVispute <gaurivispute92@gmail.com>
2025-08-12 23:36:40 +0530

    sql file

--- queries_on_DB.sql
+++ queries_on_DB.sql
@@ -1,2 +1,11 @@
-INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
-('Aarav', 'aarav@example.com', 'Male', '1995-05-14', 65000.00);
+ INSERT INTO users (name, email, gender, date_of_birth, salary) VALUES
+ (NULL, 'aara435456090@example.com', 'Male', '1995-05-14', 65000.00);
+
+alter table users add constraint unique_email unique (email);
+
+alter table users modify column name varchar(100) not null;
+
+alter table users modify column name varchar(100) null;
+
+
+select * from users;
