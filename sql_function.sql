0cc44f49dde01253fad5e41b3af0e9b1e3c9ac9b
GauriVispute <gaurivispute92@gmail.com>
2025-08-15 22:03:05 +0530

    sql file

--- sql_function.sql
+++ sql_function.sql
@@ -1,18 +1,36 @@
 select * from users;
 
+-- count() function 
 select count(*) from users where gender='Male';
 
 select count(*) from users where gender = 'Female';
-
+  
+-- min() and max() in column
 select MIN(salary) AS min_salary , MAX(salary) AS max_salary from users;
 
+-- sum() function of a column
 select sum(salary) AS total_cost_on_employees from users;
 
+-- avg() function of a column
 select avg(salary) As avg_salary from users;
 
+-- make use of group by clause
 select gender , avg(salary) As avg_salary from users group by gender;
 
+select id , gender , avg(salary) As avg_salary from users group by id;
+
+-- group by the genders for the salary 
 select gender , sum(salary) As total_salary from users group by gender;
 
 SELECT gender ,MIN(salary) AS min_salary , MAX(salary) AS max_salary from users GROUP BY gender;
+
+-- String function 
+select name , length(name) as length_of_name from users;
+
+select id , gender ,lower(name) as lower, concat(lower(name),'4567') as username, length(name) as length_of_name from users; 
+
+
+
+
+
 
