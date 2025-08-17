1c3d63668e3d351f3d4c369a4439d2b92c36736c
GauriVispute <gaurivispute92@gmail.com>
2025-08-17 21:16:37 +0530

    sql file

--- auto_commit_and_transactions.sql
+++ auto_commit_and_transactions.sql
@@ -1,7 +1,9 @@
 -- auto commit and transactions
- 
+-- (1 -> enabled  0-> disabled)
+set autocommit = 0; 
+-- rollback;
+-- delete from users where id=5;
 
-set autocommit = 0;
-
+commit;
+-- update  users  set salary = 45000  where id =1;
 select * from users;
-
